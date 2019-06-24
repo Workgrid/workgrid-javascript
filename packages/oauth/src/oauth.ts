e52862bf9460b84771bf0b4dc7b3e9f04c98a281
Jason McFeeley <jason.mcfeeley@libertymutual.com>
2019-06-24 14:30:52 -0400

    ETSWORK-3591 OAuth package update and tests

--- packages/oauth/src/oauth.ts
+++ packages/oauth/src/oauth.ts
@@ -1,51 +1,32 @@
-import logger from 'logger'
-import request  from 'utils/request'
-import { omit } from 'lodash'
 import { decode } from 'jsonwebtoken'
-import { required } from 'utils'
-import { Client } from 'client'
+import { omit } from 'lodash'
+
 
 export class OAuth{
 
-    
-    /*
-    *WHAT IT NEEDS:
-    *
-    * 
-    * 
-    * 
-    * /
-    
-    /**
-   * The parent client
-   * @private
-   * @member {Client}
-   */
-  client : Client
+  logger : any
 
-    /**
-   * The default logger
-   * @private
-   * @member {Object}
-   */
-  logger = logger
+  request : any
 
-  /**
-   * The default request library
-   * @private
-   * @member {Object}
-   */
-  request = request
+  clientId : string
+
+  clientSecret : string
+
+  scope : any
+
+  tokenUrl : string
 
   /**
    * Initialize the OAuth service
    */
   
-  constructor(client: Client) {
-    //this.logger = client.logger
-    //this.request = client.request
-    this.client = client
-
+  constructor(logger : any, request : any, clientId : string, clientSecret : string, scope : any, tokenUrl : string) {
+    this.logger = logger
+    this.request = request
+    this.clientId = clientId
+    this.clientSecret = clientSecret
+    this.tokenUrl = tokenUrl
+    this.scope = scope;
   }
 
   /**
@@ -54,36 +35,41 @@ export class OAuth{
    * @param {string} token
    * @returns {boolean}
    */
-  /*
-  isExpired(token) {
-    oauthLogger.trace('Checking token: %O', decode(token))
-    return !token || decode(token).exp < Date.now() / 1000
-  }*/
+  
+  isExpired(token : string) {
+    //this.logger.trace('Checking token: %O', decode(token))
+    var decodedToken = decode(token)
+    if(!token){return !token}
+    if(decodedToken && typeof decodedToken !== "string"){
+      return decodedToken.exp < Date.now() / 1000}
+    //unsure under what set of circumstances decode() can return type string, 
+    //nothinng listing it as a return value in documentation, but listed as a return value in the @types file
+    if(typeof decodedToken ==="string"){return false}
+    return true
+  }
 
   /**
    * Retrieve a new access token
    *
    * @returns {Promise<string>}
    */
-  /*
+  
   async getAccessToken() {
     this.logger.trace('getAccessToken()')
-    const { tokenUrl: url, scope, clientId, clientSecret } = this.client.options
 
-    const auth = { username: clientId, password: clientSecret }
-    const params = { grant_type: 'client_credentials', clientId, scope }
+    const auth = { username: this.clientId, password: this.clientSecret }
+    const params = { grant_type: 'client_credentials', clientId: this.clientId, scope: this.scope }
 
     try {
-      const response = await this.request({ method: 'post', url, auth, params })
+      const response = await this.request({ method: 'post', url : this.tokenUrl, auth, params })
       const { access_token } = response.data
-
       this.logger.trace('%O', { access_token })
       return access_token
     } catch (err) {
       this.logger.trace('Error retrieveing access_token: %s', err.message)
       throw omit(err, ['config', 'request', 'toJSON']) // may include sensitive data
     }
-  }*/
+  }
 
 
 }
