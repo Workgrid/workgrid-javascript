58eb895e2609d68ddd41fbee5f736a88e4c48611
Jason McFeeley <jason.mcfeeley@libertymutual.com>
2019-06-25 13:54:44 -0400

    ETSWORK-3591 OAuth restructuring and 100% test coverage

--- packages/oauth/src/oauth.ts
+++ packages/oauth/src/oauth.ts
@@ -12,21 +12,29 @@ export class OAuth{
 
   clientSecret : string
 
-  scope : any
+  scope : string
 
   tokenUrl : string
 
   /**
    * Initialize the OAuth service
+   * 
+   * @param {Object} params -Object containing following key-value pairs: 
+   * @param {any} params.logger 
+   * @param {any} params.request
+   * @param {string} params.clientId
+   * @param {string} params.clientSecret
+   * @param {string} params.scope
+   * @param {string} params.tokenUrl
    */
   
-  constructor(logger : any, request : any, clientId : string, clientSecret : string, scope : any, tokenUrl : string) {
-    this.logger = logger
-    this.request = request
-    this.clientId = clientId
-    this.clientSecret = clientSecret
-    this.tokenUrl = tokenUrl
-    this.scope = scope;
+  constructor(params: {logger: any, request : any, clientId : string, clientSecret : string, scope : string, tokenUrl : string}) {
+    this.logger = params.logger
+    this.request = params.request
+    this.clientId = params.clientId
+    this.clientSecret = params.clientSecret
+    this.scope = params.scope;
+    this.tokenUrl = params.tokenUrl
   }
 
   /**
@@ -37,14 +45,10 @@ export class OAuth{
    */
   
   isExpired(token : string) {
-    //this.logger.trace('Checking token: %O', decode(token))
+    this.logger.trace('Checking token: %O', decode(token))
     var decodedToken = decode(token)
-    if(!token){return !token}
     if(decodedToken && typeof decodedToken !== "string"){
-      return decodedToken.exp < Date.now() / 1000}
-    //unsure under what set of circumstances decode() can return type string, 
-    //nothinng listing it as a return value in documentation, but listed as a return value in the @types file
-    if(typeof decodedToken ==="string"){return false}
+    return decodedToken.exp < Date.now() / 1000}
     return true
   }
 
@@ -55,21 +59,18 @@ export class OAuth{
    */
   
   async getAccessToken() {
-    //this.logger.trace('getAccessToken()')
-
+    this.logger.trace('getAccessToken()')
     const auth = { username: this.clientId, password: this.clientSecret }
     const params = { grant_type: 'client_credentials', clientId: this.clientId, scope: this.scope }
 
     try {
       const response = await this.request({ method: 'post', url : this.tokenUrl, auth, params })
       const { access_token } = response.data
-      //this.logger.trace('%O', { access_token })
+      this.logger.trace('%O', { access_token })
       return access_token
     } catch (err) {
-      //this.logger.trace('Error retrieveing access_token: %s', err.message)
+      this.logger.trace('Error retrieveing access_token: %s', err.message)
       throw omit(err, ['config', 'request', 'toJSON']) // may include sensitive data
     }
   }
-
-
 }
--- packages/oauth/test/oauth.test.ts
+++ packages/oauth/test/oauth.test.ts
@@ -1,9 +1,9 @@
 import { OAuth } from "../src/oauth";
-import axios from 'axios';
+import { sign } from 'jsonwebtoken'
 
 describe("Testing Oauth", () => {
     let logger : any = console
-    let request : any = axios
+    let request : any 
     let oauth : OAuth
     let clientId : string
     let clientSecret : string
@@ -12,12 +12,12 @@ describe("Testing Oauth", () => {
     let currentGoodToken = "test"
     beforeEach(() => {
         logger = console
-        request = axios
+        request = jest.fn(function(){return {data : {access_token: "testAccessToken"}}})
         clientId = 'testClientId'
         clientSecret = 'testSecret'
         scope = 'com.test.api/testifications.all'
         tokenUrl = 'testTokenUrl'
-        oauth = new OAuth(logger,request,clientId,clientSecret,scope,tokenUrl);
+        oauth = new OAuth({logger,request,clientId,clientSecret,scope,tokenUrl});
     })
     describe("oauth", () => {
         test('Not a token is Expired', async () => {
@@ -25,14 +25,26 @@ describe("Testing Oauth", () => {
         })
 
         test('Expired token is Expired', async () => {
-            expect(oauth.isExpired('blahh')).toBe(true);
+            let access_token = sign({}, 'secret', { expiresIn: '0m' })
+            expect(oauth.isExpired(access_token)).toBe(true);
+        })
+
+        test('Unexpired token is not expired', async () => {
+            let access_token = sign({}, 'secret', { expiresIn: '1m' })
+            expect(oauth.isExpired(access_token)).toBe(false);
         })
 
         test('get access token is successful',  async () => {
             request = jest.fn(function(){return {data : {access_token: "testAccessToken"}}})
-            oauth = new OAuth(logger,request,clientId,clientSecret,scope,tokenUrl);
+            oauth = new OAuth({logger,request,clientId,clientSecret,scope,tokenUrl});
             let test = await oauth.getAccessToken()
             expect(test === "testAccessToken").toBe(true)
+        })
+
+        test('get access token catch error',  async () => {
+            request = jest.fn(function(){throw new Error("Uh Oh Spaghettios!")})
+            oauth = new OAuth({logger,request,clientId,clientSecret,scope,tokenUrl});
+            await expect(oauth.getAccessToken()).rejects.toThrow
         })
     })
 })
