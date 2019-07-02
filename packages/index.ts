efd0ff9d033203ef5dec1553c7d57032af1d93a2
William Toohey <William.Toohey@libertymutual.com>
2019-07-02 08:48:52 -0400

    ETSWORK-4156 - api-extractor is now run in individual package + generated webhook.js is now the main file of package

--- packages/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @packageDocumentation - This package is used to determine whether a given webhook is valid.
- */
-
-import { isValid } from "./webhook-validation/src/webhook"
-
-export { isValid }
