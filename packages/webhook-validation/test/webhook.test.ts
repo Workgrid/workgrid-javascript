f9c3790d413cccf0d44a8bbc3493a6d9bf7939b9
Tucker Whitehouse <Tucker.Whitehouse@LibertyMutual.com>
2019-07-11 19:20:17 -0400

    ETSWORK-921 Configure eslint and prettier (jest doesn't seem to be working though)

--- packages/webhook-validation/test/webhook.test.ts
+++ packages/webhook-validation/test/webhook.test.ts
@@ -1,26 +1,31 @@
+jest.mock('crypto')
 
-import { isValid, generateHmacDigest } from "../src/webhook"
+import isValid from '../src/webhook'
+import { createHmac } from 'crypto'
 
-describe("@workgrid/webhook-validation", () => {
-    
-    let secret : string;
-    let body : string;
-    let digest : string;
+describe('@workgrid/webhook-validation', () => {
+  let secret: string
+  let body: string
+  let digest: string
 
-    beforeAll(() => {
-        secret = "beans"
-        body = JSON.stringify({ id : 27 });
-        digest = generateHmacDigest('beans', body);
+  beforeAll(() => {
+    secret = 'secret'
+    body = 'body'
+    digest = 'digest'
+
+    const mockDigest = jest.fn(() => digest)
+    const mockUpdate = jest.fn(() => ({ digest: mockDigest }))
+    createHmac.mockImplementation(() => ({ update: mockUpdate }))
+  })
+
+  describe('isValid()', () => {
+    test('should return true if the digest matches', async () => {
+      expect(isValid(secret, body, digest)).toBe(true)
     })
 
-    describe("isValid()", () => {
-        test('should return true if the digest matches', async () => {
-            expect(isValid(secret, body, digest)).toBe(true);
-        })
-
-        test('should return false if the digest does not match',  async () => {
-            let body = JSON.stringify({ id : 72 });
-            expect(isValid(secret, body, digest)).toBe(false);
-        })
+    test('should return false if the digest does not match', async () => {
+      const body = JSON.stringify({ id: 72 })
+      expect(isValid(secret, body, digest)).toBe(false)
     })
+  })
 })
