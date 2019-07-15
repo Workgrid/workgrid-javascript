00e2e598d15b66e5fbbc9cb62db950947081120e
William Toohey <William.Toohey@libertymutual.com>
2019-07-15 10:18:50 -0400

    ETSWORK-4158 - resolved conflicts

--- packages/webhook-validation/test/webhook.test.ts
+++ packages/webhook-validation/test/webhook.test.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import isValid from '../src/webhook'
 
 jest.mock('crypto', () => ({
@@ -14,12 +13,6 @@ jest.mock('crypto', () => ({
     }))
   }))
 }))
-=======
-jest.mock('crypto')
-
-import isValid from '../src/webhook'
-import { createHmac } from 'crypto'
->>>>>>> ETSWORK-921 Configure eslint and prettier (jest doesn't seem to be working though)
 
 describe('@workgrid/webhook-validation', () => {
   let secret: string
@@ -29,15 +22,7 @@ describe('@workgrid/webhook-validation', () => {
   beforeAll(() => {
     secret = 'secret'
     body = 'body'
-<<<<<<< HEAD
     digest = 'sha256=digest'
-=======
-    digest = 'digest'
-
-    const mockDigest = jest.fn(() => digest)
-    const mockUpdate = jest.fn(() => ({ digest: mockDigest }))
-    createHmac.mockImplementation(() => ({ update: mockUpdate }))
->>>>>>> ETSWORK-921 Configure eslint and prettier (jest doesn't seem to be working though)
   })
 
   describe('isValid()', () => {
@@ -46,12 +31,8 @@ describe('@workgrid/webhook-validation', () => {
     })
 
     test('should return false if the digest does not match', async () => {
-<<<<<<< HEAD
       secret = 'notSecret'
       body = 'notBody'
-=======
-      const body = JSON.stringify({ id: 72 })
->>>>>>> ETSWORK-921 Configure eslint and prettier (jest doesn't seem to be working though)
       expect(isValid(secret, body, digest)).toBe(false)
     })
   })
