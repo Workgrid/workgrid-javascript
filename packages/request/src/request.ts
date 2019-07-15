891bf6ab3959583317a1f5616d74356a3c290e03
William Toohey <William.Toohey@libertymutual.com>
2019-07-15 09:11:48 -0400

    ETSWORK-4158 - request code works with eslint/prettier

--- packages/request/src/request.ts
+++ packages/request/src/request.ts
@@ -1,4 +1,3 @@
-
 import axios from 'axios'
 import * as oauth from 'axios-oauth-client'
 import * as tokenProvider from 'axios-token-interceptor'
@@ -6,98 +5,100 @@ import mem from 'mem'
 
 /**
  * The interface representing the options necessary for authentication.
- * 
+ *
  * @beta
  */
 export interface OAuthOptions {
-    /**
-     * The client's id
-     */
-    client_id : string,
+  /**
+   * The client's id
+   */
+  client_id: string
 
-    /**
-     * The client's secret
-     */
-    client_secret : string,
+  /**
+   * The client's secret
+   */
+  client_secret: string
 
-    /**
-     * The url to get the token from
-     */
-    url : string,
+  /**
+   * The url to get the token from
+   */
+  url: string
 
-    /**
-     * The scope the token is for
-     */
-    scope : string
-
+  /**
+   * The scope the token is for
+   */
+  scope: string
 }
 
 /**
  * The interface representing the options to be passed in for making an API request.
- * 
+ *
  * @beta
  */
 export interface APIOptions {
+  /**
+   * The options necessary for authentication
+   */
+  oauthOptions: OAuthOptions
 
-    /**
-     * The options necessary for authentication
-     */
-    oauthOptions : OAuthOptions
+  /**
+   * The type of request to be made (get, post, etc)
+   */
+  method: string
 
-    /**
-     * The type of request to be made (get, post, etc)
-     */
-    method : string,
+  /**
+   * The URL to hit
+   */
+  url: string
 
-    /**
-     * The URL to hit
-     */
-    url : string,
+  /**
+   * The body data to be supplied in the API call
+   */
+  data?: object
 
-    /**
-     * The body data to be supplied in the API call
-     */
-    data ?: object,
+  /**
+   * The URL parameters to be supplied in the API call
+   */
+  params?: object
 
-    /**
-     * The URL parameters to be supplied in the API call
-     */
-    params ?: object,
-
-    /**
-     * Extra options to be supplied to axios
-     */
-    additionalOptions ?: object
-
+  /**
+   * Extra options to be supplied to axios
+   */
+  additionalOptions?: object
 }
 
 /**
  * Returns the instance of axios for the provided client/secret/scope/url tuple.
- * 
+ *
  * @param oauthOptions - the options to provide to axios for acquiring/refreshing the OAuth token.
  */
-const createInstance = mem((oauthOptions : OAuthOptions) : any => {
-    const instance : any = axios.create()
-    const oauthClient : any = oauth.client(axios.create(), {
-        grant_type : 'client_credentials',
-        ...oauthOptions
-    })
-    const interceptor : any = oauth.interceptor(tokenProvider, oauthClient)
-    instance.interceptors.request.use(interceptor)
-    return instance
+const createInstance = mem((oauthOptions: OAuthOptions): any => {
+  const instance: any = axios.create()
+  const oauthClient: any = oauth.client(axios.create(), {
+    granttype: 'client_credentials',
+    ...oauthOptions
+  })
+  const interceptor: any = oauth.interceptor(tokenProvider, oauthClient)
+  instance.interceptors.request.use(interceptor)
+  return instance
 })
 
 /**
  * Assists in setting up OAuth authentication, forming the request to send to the Workgrid API, and actually making the request.
- * 
+ *
  * @param options - the options to provide to axios for the API call.
- * 
+ *
  * @beta
  */
-export default async function request( apiOptions : APIOptions ) : Promise<any> {
-    const oauthOptions : OAuthOptions = apiOptions.oauthOptions
-    let instance : any = createInstance(oauthOptions)
-    const options : object = Object.assign({}, apiOptions.additionalOptions, { method : apiOptions.method, data : apiOptions.data, params : apiOptions.params, url : apiOptions.url })
-    let response : object = await instance(options)
-    return response
+export default async function request(apiOptions: APIOptions): Promise<any> {
+  const oauthOptions: OAuthOptions = apiOptions.oauthOptions
+  const instance: any = createInstance(oauthOptions)
+  const options: object = Object.assign({}, apiOptions.additionalOptions, {
+    method: apiOptions.method,
+    data: apiOptions.data,
+    params: apiOptions.params,
+    url: apiOptions.url
+  })
+  const response: object = await instance(options)
+  return response
 }
--- packages/request/test/request.test.ts
+++ packages/request/test/request.test.ts
@@ -1,60 +1,78 @@
-
-import request, { APIOptions, OAuthOptions } from "../src/request"
+import request, { APIOptions, OAuthOptions } from '../src/request'
 
 import axios from 'axios'
-const MockAdapter = require('axios-mock-adapter')
+import MockAdapter from 'axios-mock-adapter'
 
-let tokenInvoked : boolean = false
+let tokenInvoked = false
 
-let mock = new MockAdapter(axios)
-mock.onAny().reply(config => {
-    if (config.url == 'https://auth.code.workgrid.com/oauth2/token') {
-        tokenInvoked = true
-        const data : string[] = config.data.split("&")
-        const id : string = data[1].split("=")[1]
-        const secret : string = data[2].split("=")[1]
-        if (id == 'will' && secret == 'secret') {
-            return [200, { 'access_token' : '24', 'expires_in' : '5000' }]
-        } else {
-            return [200, { 'access_token' : '25', 'expires_in' : '5000' }]
-        }
-    } else if (config.url == 'https://code.workgrid.com/v2/jobs') {
-        const token : string = config.headers.Authorization.split(" ")[1]
-        if (token == '24') {
-            return [200, 'Success!']
-        } else {
-            return [403]
-        }
+const mock = new MockAdapter(axios)
+mock.onAny().reply((config: any) => {
+  if (config.url == 'https://auth.code.workgrid.com/oauth2/token') {
+    tokenInvoked = true
+    const data: string[] = config.data.split('&')
+    const id: string = data[1].split('=')[1]
+    const secret: string = data[2].split('=')[1]
+    if (id == 'will' && secret == 'secret') {
+      /* eslint-disable-next-line @typescript-eslint/camelcase */
+      return [200, { access_token: '24', expires_in: '5000' }]
+    } else {
+      /* eslint-disable-next-line @typescript-eslint/camelcase */
+      return [200, { access_token: '25', expires_in: '5000' }]
     }
+  } else {
+    const token: string = config.headers.Authorization.split(' ')[1]
+    if (token == '24') {
+      return [200, 'Success!']
+    } else {
+      return [403]
+    }
+  }
 })
 
-describe("@workgrid/request", () => {
+describe('@workgrid/request', () => {
+  let oauthOptions: OAuthOptions
+  let apiOptions: APIOptions
 
-    let oauthOptions : OAuthOptions
-    let apiOptions : APIOptions
+  beforeAll(() => {
+    /* eslint-disable @typescript-eslint/camelcase */
+    oauthOptions = {
+      client_id: 'will',
+      client_secret: 'secret',
+      scope: '',
+      url: 'https://auth.code.workgrid.com/oauth2/token'
+    }
+    /* eslint-enable @typescript-eslint/camelcase */
+    apiOptions = { oauthOptions: oauthOptions, url: 'https://code.workgrid.com/v2/jobs', method: 'post' }
+  })
 
-    beforeAll(() => {
-        oauthOptions = { client_id : 'will', client_secret : 'secret', scope: '', url : 'https://auth.code.workgrid.com/oauth2/token' }
-        apiOptions = { oauthOptions : oauthOptions, url : 'https://code.workgrid.com/v2/jobs', method : 'post' }
+  beforeEach(() => {
+    tokenInvoked = false
+  })
+
+  describe('request()', () => {
+    test("should return 'Success!' on successful API call", async () => {
+      const response = await request(apiOptions)
+      expect(response.data).toBe('Success!')
     })
-
-    beforeEach(() => {
-        tokenInvoked = false
+    test('should throw an error on unsuccessful API call', async () => {
+      /* eslint-disable @typescript-eslint/camelcase */
+      const newOAuthOptions: OAuthOptions = {
+        client_id: 'notWill',
+        client_secret: 'notSecret',
+        scope: '',
+        url: 'https://auth.code.workgrid.com/oauth2/token'
+      }
+      /* eslint-enable @typescript-eslint/camelcase */
+      const newAPIOptions: APIOptions = {
+        oauthOptions: newOAuthOptions,
+        url: 'https://code.workgrid.com/v2/jobs',
+        method: 'post'
+      }
+      await expect(request(newAPIOptions)).rejects.toThrowError('Request failed with status code 403')
     })
-
-    describe("request()", () => {
-        test("should return 'Success!' on successful API call", async () => {
-            let response = await request(apiOptions)
-            expect(response.data).toBe('Success!')
-        })
-        test("should throw an error on unsuccessful API call", async () => {
-            const newOAuthOptions : OAuthOptions = { client_id : 'notWill', client_secret: 'notSecret', scope: '', url : 'https://auth.code.workgrid.com/oauth2/token'}
-            const newAPIOptions : APIOptions = { oauthOptions : newOAuthOptions, url : 'https://code.workgrid.com/v2/jobs', method : 'post' }
-            await expect(request(newAPIOptions)).rejects.toThrowError('Request failed with status code 403')
-        })
-        test("new OAuth token should not be retrieved when current token is still valid", async () => {
-            await request(apiOptions)
-            expect(tokenInvoked).toBe(false)
-        })
+    test('new OAuth token should not be retrieved when current token is still valid', async () => {
+      await request(apiOptions)
+      expect(tokenInvoked).toBe(false)
     })
+  })
 })
