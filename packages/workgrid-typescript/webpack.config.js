28f6567d64747eb9dcbda2f9abdf43fdbd1c0f3f
Tucker Whitehouse <Tucker.Whitehouse@LibertyMutual.com>
2019-09-19 17:56:14 -0400

    Add .npmrc, fix yarn.lock, move devDependencies to root package and add babel-plugin-lodash!

--- packages/workgrid-typescript/webpack.config.js
+++ packages/workgrid-typescript/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
           options: {
             compact: true,
             presets: [['@babel/preset-env'], ['@babel/preset-typescript']],
-            plugins: ['@babel/plugin-proposal-class-properties']
+            plugins: ['@babel/plugin-proposal-class-properties', 'babel-plugin-lodash']
           }
         }
       },
