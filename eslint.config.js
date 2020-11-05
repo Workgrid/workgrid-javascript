5af9aad386afd45e598ba78fb4b6dc16313ad408
Tucker Whitehouse <Tucker.Whitehouse@LibertyMutual.com>
2020-11-04 21:51:52 -0500

    Add and configure eslint-plugin-header

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2020 Workgrid Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -6,6 +22,31 @@ module.exports = {
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
   ],
+  plugins: ['header'],
+  rules: {
+    'header/header': [
+      'error',
+      'block',
+      [
+        `*`,
+        ` * Copyright ${new Date().getFullYear()} Workgrid Software`,
+        ` *`,
+        ` * Licensed under the Apache License, Version 2.0 (the "License");`,
+        ` * you may not use this file except in compliance with the License.`,
+        ` * You may obtain a copy of the License at`,
+        ` *`,
+        ` *     http://www.apache.org/licenses/LICENSE-2.0`,
+        ` *`,
+        ` * Unless required by applicable law or agreed to in writing, software`,
+        ` * distributed under the License is distributed on an "AS IS" BASIS,`,
+        ` * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.`,
+        ` * See the License for the specific language governing permissions and`,
+        ` * limitations under the License.`,
+        ` `,
+      ],
+      2,
+    ],
+  },
   overrides: [
     {
       files: ['*.config.js', 'scripts/*.js'],
