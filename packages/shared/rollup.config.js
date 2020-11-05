15697f52fe98af9355785544bef50d132a3d78e9
Tucker Whitehouse <Tucker.Whitehouse@LibertyMutual.com>
2020-11-04 22:03:16 -0500

    Include the license banner in built files

--- packages/shared/rollup.config.js
+++ packages/shared/rollup.config.js
@@ -38,6 +38,25 @@ const semver = require('semver')
 const readPkgUp = require('read-pkg-up')
 const { get, upperFirst, camelCase } = require('lodash')
 
+const outdent = require('outdent')
+const license = outdent`
+  /**
+   * Copyright ${new Date().getFullYear()} Workgrid Software
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   *     http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   */
+`
+
 module.exports = (input) => {
   const source = path.resolve(process.cwd(), input)
   const cwd = path.dirname(source)
@@ -57,6 +76,7 @@ module.exports = (input) => {
           format: 'cjs',
           sourcemap: true,
           exports: 'auto',
+          banner: license,
         },
       ],
       preserveSymlinks: true, // yarn workspaces
@@ -121,6 +141,7 @@ module.exports = (input) => {
           name: amdName,
           sourcemap: true,
           exports: 'auto',
+          banner: license,
         },
       ],
       preserveSymlinks: true, // yarn workspaces
