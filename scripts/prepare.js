da5c3f78c6115231ae77c241514ec50b4908fdce
Tucker Whitehouse <Tucker.Whitehouse@LibertyMutual.com>
2020-11-04 21:52:55 -0500

    Run lint (add license headers)

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,5 +1,18 @@
-// Currently unused - was experimenting with updating all the versions for a canary release prior to running the release script
-// This would allow our github workflow to conditionally run the prepare script, and then always run release :)
+/**
+ * Copyright 2020 Workgrid Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 const path = require('path')
 const { values } = require('lodash')
