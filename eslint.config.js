eafb3fd30f1988a10abe09491c147e80a189de27
Tucker Whitehouse <Tucker.Whitehouse@libertymutual.com>
2021-03-20 12:38:58 -0400

    Upgrade most workspace dependencies (latest typescript!)

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
   ],
   plugins: ['header'],
   rules: {
